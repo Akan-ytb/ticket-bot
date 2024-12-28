2679f1010a83cd7942aa6d8dc9a34f1a84199196
Akan <jsuispashumain2@gmail.com>
2024-12-28 12:55:13 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,50 @@
+<h1 align="center">Ticket Bot 🎫</h1>
+
+Ticket Bot est un bot Discord conçu pour gérer les canaux de tickets. Il est gratuit et open-source, créé comme une alternative aux bots Discord qui facturent des fonctionnalités premium.
+
+Fonctionnalités 🔥
+Voici quelques fonctionnalités de ce système.
+
+Hautement personnalisable
+Vous pouvez personnaliser tout ce que vous voulez concernant le bot, ses messages, ses embeds, ses boutons et bien plus.
+
+Facile à configurer
+Très simple à configurer, il suffit d’utiliser la commande /config et elle affichera un embed avec des boutons pour configurer le bot.
+
+Système multi-serveurs
+Le bot est optimisé pour fonctionner avec plusieurs serveurs en même temps, vous n’aurez donc pas besoin d'héberger un bot pour chaque serveur.
+
+Système de transcription
+Lorsque vous fermez un ticket, le bot vous demande si vous souhaitez sauvegarder la transcription dans un canal que vous aurez défini. Si c’est le cas, le bot enverra un fichier .html contenant un résumé de tous les messages envoyés dans le canal.
+
+Dépendances 🔗
+VPS (Ubuntu, Debian, CentOS) 🐧
+NodeJS v16+ 🛠
+Token Discord Bot 🤖
+MongoDB 🥭
+Installation 📦
+Pour installer le bot, vous devez disposer d'un VPS avec les exigences ci-dessus. (Si vous n'avez pas de base de données MongoDB, vous pouvez en créer une gratuitement sur Mongo Atlas.)
+
+Assurez-vous d'avoir NodeJS v16+ installé. N'oubliez pas de renommer le fichier config.example.json en config.json.
+
+sh
+Copier le code
+git clone https://github.com/akanytb/ticket-bot.git
+cd ticket-bot
+npm install
+npm start
+À faire 🚧
+ Ajouter la prise en charge d'autres bases de données comme (MySQL, FlatFile, etc.)
+Support ? 💁🏻
+Si vous ne comprenez pas quelque chose ou si vous avez une question à propos du Ticket Bot, vous pouvez [rejoindre notre Discord](https://discord.gg/xenosclxn)
+
+Montrez votre soutien 💙
+Donnez une ⭐️ si ce projet vous a aidé !
+
+Licence 📄
+Ticket Bot est sous licence MIT.
+
+Ce n’est pas un produit officiel de Discord. Il n'est ni affilié à ni approuvé par Discord Inc.
+
+© 2022 - Akanytb.
+
---
+++ commands/tickets/alert.js
@@ -0,0 +1,73 @@
+const { Client, CommandInteraction, MessageEmbed } = require("discord.js");
+const { error } = require("../../controllers/logger");
+const { isTicket } = require("../../controllers/ticketChecks");
+
+module.exports = {
+	name: "alert",
+	description: "Alerter un utilisateur pour qu'il réponde à son ticket",
+	type: 'CHAT_INPUT',
+	/**
+	 *
+	 * @param {Client} client
+	 * @param {CommandInteraction} interaction
+	 * @param {String[]} args
+	 */
+	run: async (client, interaction, args) => {
+		const ticketData = await isTicket(interaction);
+		if (!ticketData) {
+			return interaction.reply({embeds: [
+				new MessageEmbed()
+					.setTitle("Système de tickets \❌")
+					.setDescription(client.languages.__("errors.channel_without_ticket"))
+					.setColor("RED")
+			], ephemeral: true});
+		}
+		
+		const user = interaction.guild.members.cache.get(ticketData.ownerID);
+		if (!user) {
+			error(`Could not find user with ID ${ticketData.ownerID}`);
+			return interaction.reply({embeds: [
+				new MessageEmbed()
+					.setTitle("Système de tickets \❌")
+					.setDescription("Utilisateur introuvable.")
+					.setColor("RED")
+			], ephemeral: true});
+		}
+
+		try {
+			await user.send({embeds: [
+				new MessageEmbed()
+					.setTitle("Système de tickets \✅")
+					.setDescription(client.languages.__mf("commands.alert.user_message", {
+						user_mention: `<@${ticketData.ownerID}>`,
+						user_tag: user.user.tag,
+						channel_id: ticketData.channelID,
+						channel_name: interaction.channel.name,
+						link: `https://discordapp.com/channels/${interaction.guild.id}/${ticketData.channelID}`,
+						direct_link: `[Direct Link](https://discordapp.com/channels/${interaction.guild.id}/${ticketData.channelID})`,
+						openSince: `<t:${(Math.floor(ticketData.dateCreated / 1000))}:R>`
+					}))
+					.setColor("GREEN")
+			]});
+		} catch (error_) {
+			error(error_);
+			return interaction.reply({embeds: [
+				new MessageEmbed()
+					.setTitle("Système de tickets \❌")
+					.setDescription("Impossible d'envoyer un DM à l'utilisateur.")
+					.setColor("RED")
+			], ephemeral: true});
+		}
+
+		return interaction.reply({embeds: [
+			new MessageEmbed()
+				.setTitle("Système de tickets \✅")	
+				.setDescription(client.languages.__mf("commands.alert.staff_message", {
+					user_mention: `<@${interaction.user.id}>`,
+					channel_id: ticketData.channelID,
+					direct_link: `[Direct Link](https://discordapp.com/channels/${interaction.guild.id}/${ticketData.channelID})`,
+				}))
+				.setColor("GREEN")
+		]});
+	},
+};
